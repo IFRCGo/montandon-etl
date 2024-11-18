6218511652bf5180d79e5fceca43be04faf6cf63
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2024-11-18 16:47:04 +0545

    Remove old table and add different table for Logs and Extraction.

--- apps/etl/admin.py
+++ apps/etl/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import GDACS
+from .models import ExtractionLog, ExtractionData
 
-admin.site.register(GDACS)
+admin.site.register(ExtractionData)
+admin.site.register(ExtractionLog)
--- apps/etl/models.py
+++ apps/etl/models.py
@@ -4,19 +4,33 @@ from apps.common.models import UserResource
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class ExtractionLog(UserResource):
+    class Source(models.IntegerChoices):
+        GDACS = 1, _("Gdacs")
+        PDC = 2, _("pdc")
 
-class GDACS(UserResource):
     class Status(models.IntegerChoices):
-        IN_PROGRESS = 1, _("In progress")
-        PENDING = 2, _("Pending")
+        PENDING = 1, _("Pending")
+        IN_PROGRESS = 2, _("In progress")
         SUCCESS = 3, _("Success")
+        FAILED = 4, _("Failed")
 
-    raw_data = models.JSONField(verbose_name=_("raw data"), default=dict)
-    log = models.TextField(verbose_name=_("log"), blank=True)
-    response_time = models.CharField(verbose_name=_("response time"), blank=True, max_length=100)
+    uuid = models.UUIDField()
+    source = models.IntegerField(verbose_name=_("source"), choices=Source.choices)
+    url = models.URLField(blank=True)
+    attempt_no = models.IntegerField(blank=True)
+    req_started_datetime = models.DateTimeField(blank=True)
+    response_time = models.TimeField(blank=True)
     response_code = models.IntegerField(verbose_name=_("response code"), blank=True)
     status = models.IntegerField(verbose_name=_("status"), choices=Status.choices)
 
-    class Meta:
-        verbose_name = _("gdac")
-        verbose_name_plural = _("gdacs")
+class ExtractionData(UserResource):
+    class ResponseDataType(models.IntegerChoices):
+        JSON = 1, _("Json")
+        CSV = 2, _("CSV")
+        TEXT = 3, _("Text")
+
+    extraction_log = models.ForeignKey(ExtractionLog, on_delete=models.CASCADE, related_name='extraction_log')
+    uuid=models.UUIDField()
+    raw_data = models.TextField(verbose_name=_("raw data"), blank=True)
+    response_data_type = models.IntegerField(choices=ResponseDataType.choices)
