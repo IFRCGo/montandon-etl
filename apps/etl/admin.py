53474584da69da4e71d8702540df294e2c32e51b
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2024-11-25 15:24:48 +0545

    Modify admin table for Extraction Data.
      - save sarap data in html format.

--- apps/etl/admin.py
+++ apps/etl/admin.py
@@ -7,9 +7,11 @@ from .models import ExtractionData
 @admin.register(ExtractionData)
 class ExtractionDataAdmin(admin.ModelAdmin):
     list_display = (
+        "id",
         "source",
         "resp_code",
         "status",
-        "resp_type",
+        "parent__id",
+        "resp_data_type",
     )
     list_filter = ("status",)
