af81446d220f4fd3129065688dc119ccfbc4a019
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2024-11-26 17:49:23 +0545

    Set all fields to readonly in admin.
      - Save whole html file into response data for html response.

--- apps/etl/admin.py
+++ apps/etl/admin.py
@@ -6,6 +6,13 @@ from .models import ExtractionData
 
 @admin.register(ExtractionData)
 class ExtractionDataAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        # Use the model's fields to populate readonly_fields
+        if obj:  # If the object exists (edit page)
+            return [field.name for field in self.model._meta.fields]
+        return []
+
+    # readonly_fields = ('__all__',)
     list_display = (
         "id",
         "source",
@@ -13,5 +20,8 @@ class ExtractionDataAdmin(admin.ModelAdmin):
         "status",
         "parent__id",
         "resp_data_type",
+        "created_at",
     )
     list_filter = ("status",)
+    autocomplete_fields = ["parent"]
+    search_fields = ["parent"]
