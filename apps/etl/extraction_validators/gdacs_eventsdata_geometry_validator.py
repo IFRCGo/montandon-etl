caf58210a36f5c91cd527cff44e8f12505e5fe58
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2024-11-29 13:32:52 +0545

    Add the validation for events geometry data;

---
+++ apps/etl/extraction_validators/gdacs_eventsdata_geometry_validator.py
@@ -0,0 +1,62 @@
+from typing import List, Optional, Union
+from pydantic import BaseModel, HttpUrl
+from datetime import datetime
+
+class URLDetails(BaseModel):
+    geometry: HttpUrl
+    report: HttpUrl
+    details: HttpUrl
+
+class SeverityData(BaseModel):
+    severity: float
+    severitytext: str
+    severityunit: str
+
+class AffectedCountry(BaseModel):
+    iso3: str
+    countryname: str
+
+class Properties(BaseModel):
+    eventtype: str
+    eventid: int
+    episodeid: int
+    eventname: str
+    glide: Optional[str]
+    name: str
+    description: str
+    htmldescription: str
+    icon: HttpUrl
+    iconoverall: HttpUrl
+    url: URLDetails
+    alertlevel: str
+    alertscore: float
+    episodealertlevel: str
+    episodealertscore: float
+    istemporary: str
+    iscurrent: str
+    country: str
+    fromdate: datetime
+    todate: datetime
+    datemodified: datetime
+    iso3: str
+    source: str
+    sourceid: str
+    polygonlabel: str
+    Class: str
+    countryonland: str
+    affectedcountries: List[AffectedCountry]
+    severitydata: SeverityData
+
+class Geometry(BaseModel):
+    type: str
+    coordinates: Union[List[float], List[List[List[float]]]]
+
+class Feature(BaseModel):
+    type: str
+    bbox: List[float]
+    geometry: Geometry
+    properties: Properties
+
+class GdacsEventsGeometryData(BaseModel):
+    type: str
+    features: List[Feature]
