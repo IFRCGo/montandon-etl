d2bba2af6025871f099f2114eed3a9aebb44e6ab
rup <rup.rajbanshi@togglecorp.com>
2024-12-15 16:34:35 +0545

    Code clean up.

--- apps/etl/extract.py
+++ apps/etl/extract.py
@@ -1,5 +1,6 @@
 import requests
-import logging
+
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import ExtractionData
 
@@ -15,7 +16,7 @@ class Extraction:
             return "html"
         return "txt"
 
-    def pull_data(self, source: int, retry_count: int, attempt_no: int = 1, timeout: int = 30, ext_object_id: int = None):
+    def pull_data(self, source: int, retry_count: int, timeout: int = 30, ext_object_id: int = None):
         resp_status = ExtractionData.Status.IN_PROGRESS
         source_validation_status = ExtractionData.ValidationStatus.NO_VALIDATION
 
@@ -26,22 +27,19 @@ class Extraction:
                 instance_obj.resp_code = resp_status
                 instance_obj.save(update_fields=["resp_code"])
             except ExtractionData.DoesNotExist:
-                logging.error(f"ExtractionData object with ID {ext_object_id} not found")
+                raise ObjectDoesNotExist("ExtractionData object with ID {ext_object_id} not found")
 
         try:
-            print("Retry number: ", retry_count)
             response = requests.get(self.url, timeout=timeout)
             resp_type = response.headers.get("Content-Type", "")
             file_extension = self._get_file_extension(resp_type)
 
-
             # Try fetching the data in case of failure
             if response.status_code != 200:
-                # object create without validation and status FAILED
                 data = {
                     "source": source,
                     "url": self.url,
-                    "attempt_no": attempt_no + retry_count,
+                    "attempt_no": retry_count,
                     "resp_code": response.status_code,
                     "status": ExtractionData.Status.FAILED,
                     "resp_data": None,
@@ -52,8 +50,8 @@ class Extraction:
                     "resp_text": response.text,
                     }
                 instance_obj = ExtractionData.objects.get(id=ext_object_id)
-                instance_obj.resp_code = response.status_code
-                instance_obj.save(update_fields=["resp_code"])
+#                instance_obj.resp_code = response.status_code
+#                instance_obj.save(update_fields=["resp_code"])
 
                 for key, value in data.items():
                     setattr(instance_obj, key, value)
@@ -61,19 +59,15 @@ class Extraction:
 
                 raise Exception()
 
+            elif response.status_code == 204:
+                source_validation_status = ExtractionData.ValidationStatus.NO_DATA
 
             resp_status = ExtractionData.Status.SUCCESS
-            source_validation_status = ExtractionData.ValidationStatus.NO_VALIDATION
-
-                # elif response.status_code == 204:
-                #     resp_status = ExtractionData.Status.SUCCESS
-                #     source_validation_status = ExtractionData.ValidationStatus.NO_DATA
-
 
             return {
                     "source": source,
                     "url": self.url,
-                    "attempt_no": attempt_no + retry_count,
+                    "attempt_no": retry_count,
                     "resp_code": response.status_code,
                     "status": resp_status,
                     "resp_data": response,
@@ -81,23 +75,8 @@ class Extraction:
                     "file_extension": file_extension,
                     "source_validation_status": source_validation_status,
                     "content_validation": "",
-                    "resp_text": response.text,
+                    "resp_text": "",
                 }
 
-
-        except Exception as e:
-            raise Exception() 
-
-            return {
-                "source": source,
-                "url": self.url,
-                "attempt_no": retry_count,
-                "resp_code": 0,
-                "status": ExtractionData.Status.FAILED,
-                "source_validation_status": source_validation_status,
-                "resp_data": None,
-                "resp_data_type": "",
-                "file_extension": None,
-                "content_validation": "",
-                "resp_text": str(e),
-            }
+        except Exception:
+            raise Exception()
