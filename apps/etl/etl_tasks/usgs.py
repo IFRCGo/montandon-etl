d46dff4bb23b278f7a15979cc2e284604a75c21d
sudan45 <sudan1bhandari@gmail.com>
2025-01-29 15:22:19 +0545

    Add extraction and transformation in single command:

--- apps/etl/etl_tasks/usgs.py
+++ apps/etl/etl_tasks/usgs.py
@@ -1,10 +1,10 @@
 from celery import shared_task
 
 from apps.etl.extraction.sources.usgs.extract import (
-    import_hazard_data as import_usgs_data,
+    import_hazard_data as extraction_and_transform_usgs_data,
 )
 
 
 @shared_task
-def import_usgs_hazard_data(**kwargs):
-    import_usgs_data()
+def import_and_transform_usgs_data(**kwargs):
+    extraction_and_transform_usgs_data()
--- apps/etl/management/commands/extract_usgs_data.py
+++ apps/etl/management/commands/extract_usgs_data.py
@@ -2,8 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from apps.etl.etl_tasks.usgs import import_usgs_data
-from apps.etl.transform.sources.usgs import transform_usgs_event_data
+from apps.etl.etl_tasks.usgs import import_and_transform_usgs_data
 
 logger = logging.getLogger(__name__)
 
@@ -12,5 +11,4 @@ class Command(BaseCommand):
     help = "Import data from usgs"
 
     def handle(self, *args, **options):
-        data = import_usgs_data()
-        transform_usgs_event_data(data)
+        import_and_transform_usgs_data.delay()
