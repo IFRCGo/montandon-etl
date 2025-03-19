d25da6252c97a0263e1cbd508fbb355f4ef669a3
sudan45 <sudan1bhandari@gmail.com>
2025-03-19 13:00:40 +0545

    Update index in trace_id

--- apps/common/models.py
+++ apps/common/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Resource(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    trace_id = models.UUIDField(editable=False)
     # Typing
     id: int
     pk: int
