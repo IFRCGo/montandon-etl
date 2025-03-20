4bd2e8e595677dbbc8e5d894c7c7afda8646d152
thenav56 <ayernavin@gmail.com>
2025-03-20 23:00:36 +0545

    Delete PeriodicTask not defined in codebase
    - Filter using tasks in `apps.`
    - Also ignore task with name starting with `manual:`

--- main/celery.py
+++ main/celery.py
@@ -3,6 +3,7 @@ import os
 from logging.config import dictConfig
 
 from celery import Celery, signals
+from django.db import models
 
 from utils.common import get_all_modules
 
@@ -23,6 +24,37 @@ app.autodiscover_tasks()
 app.autodiscover_tasks(get_all_modules("apps/etl/etl_tasks"))
 
 app.conf.beat_schedule = BEAT_SCHEDULES
+
+
+logger = logging.getLogger(__name__)
+
+
+@signals.beat_init.connect
+def clean_up_periodic_tasks(**_):
+    try:
+        from django_celery_beat.models import PeriodicTask
+
+        obsolute_tasks_qs = PeriodicTask.objects.filter(
+            task__startswith="apps.",  # Our tasks
+        ).exclude(
+            models.Q(
+                name__in=list(BEAT_SCHEDULES.keys()),
+            )
+            | models.Q(
+                name__startswith="manual:",  # Lets filter-out if it has `manual:` at the start
+            )
+        )
+
+        obsolute_tasks = list(obsolute_tasks_qs)
+        if obsolute_tasks:
+            for task in obsolute_tasks:
+                logger.warning(f"Task to delete: {task.name}")
+
+            deleted_periodic_task = obsolute_tasks_qs.delete()
+            logger.warning(f"Deleted tasks not defined in the codebase: {deleted_periodic_task}")
+    except Exception:
+        logger.error("Failed to clean-up PeriodicTasks", exc_info=True)
+
 
 @signals.setup_logging.connect
 def config_loggers(**_):
