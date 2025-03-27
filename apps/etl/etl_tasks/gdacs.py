735ebd83c7ded36e59f056ca9d8075a5eb7befce
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2025-03-27 10:06:16 +0545

    Class based for gdacs.   - Handle historical data for gdacs.

--- apps/etl/etl_tasks/gdacs.py
+++ apps/etl/etl_tasks/gdacs.py
@@ -1,167 +1,145 @@
+import datetime
 import json
-import logging
-from datetime import datetime, timedelta
+from datetime import datetime as dt
 
 import requests
+import requests_cache
 from celery import chain, shared_task
+from celery.utils.log import get_task_logger
 
-from apps.etl.extraction.sources.base.extract import Extraction
-from apps.etl.extraction.sources.base.utils import store_extraction_data
 from apps.etl.extraction.sources.gdacs.extract import (
-    fetch_event_data,
-    fetch_gdacs_geometry_data,
-    validate_source_data,
+    GdacsEventExtractionInputMetadata,
+    GdacsExtraction,
+    GdacsExtractionInputMetadata,
 )
-from apps.etl.models import ExtractionData, HazardType, get_trace_id
-from apps.etl.transform.sources.gdacs import (
-    transform_event_data,
-    transform_geo_data,
-    transform_impact_data,
-)
+from apps.etl.models import ExtractionData, HazardType
+from apps.etl.transform.sources.gdacs import GDACSTransformHandler
 from main.configs import etl_config
+from main.logging import log_extra
 
-logger = logging.getLogger(__name__)
+logger = get_task_logger(__name__)
+
+session = requests_cache.CachedSession(
+    "gdacs_request",
+    expire_after=-1,
+    allowable_codes=[200, 204],
+)
+
+HAZARDS = [
+    (HazardType.EARTHQUAKE, 64),
+    (HazardType.CYCLONE, 64),
+    (HazardType.FLOOD, 64),
+    (HazardType.DROUGHT, 64),
+    (HazardType.WILDFIRE, 64),
+    (HazardType.VOLCANO, 64),
+    (HazardType.TSUNAMI, 64),
+]
+
+URL = f"{etl_config.GDACS_URL}/gdacsapi/api/events/geteventlist/SEARCH"
+
+
+@shared_task
+def extract_and_transform_data(url, metadata, input_metadata_class):
+    try:
+        parent_extraction_id = GdacsExtraction.task(
+            url=URL, metadata=metadata, input_metadata_class=GdacsExtractionInputMetadata
+        )
+        hazard_type = metadata["eventlist"]
+
+        if parent_extraction_id:
+            base_instance = ExtractionData.objects.get(id=parent_extraction_id)
+            base_response_data = json.loads(base_instance.resp_data.read())
+
+            for feature in base_response_data["features"]:
+                event_id = feature["properties"]["eventid"]
+                event_detail_url = f"{etl_config.GDACS_URL}/gdacsapi/api/events/geteventdata"
+                event_params = GdacsEventExtractionInputMetadata(eventtype=hazard_type, eventid=event_id, episodeid=None)
+
+                event_detail_base_extraction_id = GdacsExtraction.task(
+                    url=event_detail_url,
+                    metadata=event_params.model_dump(),
+                    parent_id=base_instance.id,
+                    input_metadata_class=GdacsEventExtractionInputMetadata,
+                )
+
+                chain(extract.s(event_detail_base_extraction_id), GDACSTransformHandler.task.s()).apply_async()
+
+    except requests.exceptions.RequestException:
+        logger.error(
+            "Extraction failed",
+            exc_info=True,
+            extra=log_extra({"metadata": metadata}),
+        )
+
+
+@shared_task
+def extract(extraction_object_id):
+    """
+    Import hazard data from gdacs api
+    """
+    event_instance = ExtractionData.objects.get(id=extraction_object_id)
+    event_response_data = json.loads(event_instance.resp_data.read())
+
+    for episode_data in event_response_data["properties"]["episodes"]:
+        event_episode_url = episode_data["details"]
+        event_episode_extraction_id = GdacsExtraction.handle_extraction(
+            params=None,
+            url=event_episode_url,
+            parent_id=event_instance.id,
+            source=ExtractionData.Source.GDACS,
+            headers={"accept": "application/json"},
+        )
+        event_episode_instance_id = ExtractionData.objects.get(id=event_episode_extraction_id)
+        event_episode_response_data = json.loads(event_episode_instance_id.resp_data.read())
+
+        geometry_episode_url = event_episode_response_data["properties"]["url"]["geometry"]
+        GdacsExtraction.handle_extraction(
+            params=None,
+            url=geometry_episode_url,
+            parent_id=event_episode_instance_id.id,
+            source=ExtractionData.Source.GDACS,
+            headers={"accept": "application/json"},
+        )
+
+        return event_instance.id
+
+
+@shared_task
+def _ext_and_transform_gdacs_latest_data(hazard: HazardType, size):
+    from apps.etl.etl_tasks.run_gdacs_historical import deep_dive
+
+    ext_object = (
+        ExtractionData.objects.filter(
+            source=ExtractionData.Source.GDACS,
+            hazard_type=hazard,
+            status=ExtractionData.Status.SUCCESS,
+            resp_data__isnull=False,
+        )
+        .order_by("-created_at")
+        .first()
+    )
+
+    if ext_object:
+        start_date = ext_object.created_at.date()
+    else:
+        start_date = etl_config.GDACS_START_DATE
+
+    end_date = dt.today().date()
+
+    deep_dive(session, hazard, start_date, end_date, size, "")
 
 
 @shared_task
 def ext_and_transform_gdacs_latest_data():
-    def _ext_and_transform_data(hazard_type: str, hazard_type_str: str):
-        ext_object = (
-            ExtractionData.objects.filter(
-                source=ExtractionData.Source.GDACS,
-                hazard_type=hazard_type,
-                status=ExtractionData.Status.SUCCESS,
-                resp_data__isnull=False,
-            )
-            .order_by("-created_at")
-            .first()
-        )
-        if ext_object:
-            from_date = ext_object.created_at.date()
-        else:
-            # Fetch data up to one week at the begining.
-            from_date = etl_config.GDACS_START_DATE
-
-        to_date = datetime.now().date()
-        ext_and_transform_gdacs_data.delay(hazard_type, hazard_type_str, from_date, to_date)
-
-    _ext_and_transform_data("EQ", HazardType.EARTHQUAKE)
-    _ext_and_transform_data("TC", HazardType.CYCLONE)
-    _ext_and_transform_data("FL", HazardType.FLOOD)
-    _ext_and_transform_data("DR", HazardType.DROUGHT)
-    _ext_and_transform_data("WF", HazardType.WILDFIRE)
-    _ext_and_transform_data("VO", HazardType.VOLCANO)
-    _ext_and_transform_data("TS", HazardType.TSUNAMI)
+    for hazard, size in HAZARDS:
+        _ext_and_transform_gdacs_latest_data(hazard, size)
 
 
-def ext_and_transform_gdacs_historical_data(hazard_type: str, hazard_type_str: str):
-    # Start from 2000-01-01
-    start_date = datetime(2000, 1, 1)
-    end_date = datetime(datetime.now().year, datetime.now().month, datetime.now().day)
+@shared_task
+def ext_and_transform_gdacs_historical_data():
+    from apps.etl.etl_tasks.run_gdacs_historical import deep_dive
 
-    while start_date <= end_date:
-        month_start = start_date.strftime("%Y-%m-%d")
-        month_end = (start_date + timedelta(days=31)).replace(day=1) - timedelta(days=1)
-        month_end = month_end.strftime("%Y-%m-%d")
-
-        ext_and_transform_gdacs_data.delay(hazard_type, hazard_type_str, month_start, month_end)
-
-        start_date += timedelta(days=31)
-        start_date = start_date.replace(day=1)
-
-
-@shared_task(bind=True, max_retries=3, default_retry_delay=5)
-def ext_and_transform_gdacs_data(self, hazard_type: str, hazard_type_str: str, from_date: str, to_date: str, **kwargs):
-    """
-    Import hazard data from gdacs api
-    """
-    logger.info(f"Importing {hazard_type} data")
-
-    gdacs_url = f"{etl_config.GDACS_URL}/gdacsapi/api/events/geteventlist/SEARCH?eventlist={hazard_type}&fromDate={from_date}&toDate={to_date}&alertlevel=Green;Orange;Red"  # noqa: E501
-
-    # Create a Extraction object in the begining
-    instance_id = kwargs.get("instance_id", None)
-    retry_count = kwargs.get("retry_count", None)
-
-    if instance_id:
-        gdacs_instance = ExtractionData.objects.get(id=instance_id)
-    else:
-        gdacs_instance = ExtractionData.objects.create(
-            source=ExtractionData.Source.GDACS,
-            status=ExtractionData.Status.PENDING,
-            source_validation_status=ExtractionData.ValidationStatus.NO_VALIDATION,
-            trace_id=get_trace_id(None),
-            hazard_type=hazard_type_str,
-            attempt_no=0,
-            resp_code=0,
-        )
-
-    # Extract the data from api.
-    gdacs_extraction = Extraction(url=gdacs_url)
-    response = None
-    try:
-        response = gdacs_extraction.pull_data(
-            source=ExtractionData.Source.GDACS,
-            ext_object_id=gdacs_instance.id,
-            retry_count=retry_count if retry_count else 1,
-        )
-    except requests.exceptions.RequestException as exc:
-        self.retry(exc=exc, kwargs={"instance_id": gdacs_instance.id, "retry_count": self.request.retries})
-
-    if response:
-        resp_data_content = response["resp_data"].content
-
-        # decode the byte object(response data) into json
-        try:
-            resp_data_json = json.loads(resp_data_content.decode("utf-8"))
-        except json.JSONDecodeError as e:
-            logger.info(f"JSON decode error: {e}")
-            resp_data_json = {}
-
-        # Save the extracted data into the existing gdacs object
-        gdacs_instance = store_extraction_data(
-            source=ExtractionData.Source.GDACS,
-            response=response,
-            validate_source_func=validate_source_data,
-            instance_id=gdacs_instance.id,
-        )
-
-        # Fetch geometry and population exposure data
-        if gdacs_instance.resp_code == 200 and gdacs_instance.status == ExtractionData.Status.SUCCESS and resp_data_json:
-            for feature in resp_data_json["features"]:
-                event_id = feature["properties"]["eventid"]
-                episode_id = feature["properties"]["episodeid"]
-                footprint_url = feature["properties"]["url"]["geometry"]
-                if hazard_type == HazardType.CYCLONE and event_id and episode_id:
-                    footprint_url = f"{etl_config.GDACS_URL}/contentdata/resources/{hazard_type_str}/{event_id}/geojson_{event_id}_{episode_id}.geojson"  # noqa: E501
-
-                event_workflow = chain(
-                    fetch_event_data.s(
-                        parent_id=gdacs_instance.id,
-                        event_id=event_id,
-                        hazard_type=hazard_type,
-                    ),
-                    transform_event_data.s(),
-                )
-                event_result = event_workflow.apply_async()
-
-                geo_workflow = chain(
-                    fetch_gdacs_geometry_data.s(
-                        parent_id=gdacs_instance.id,
-                        footprint_url=footprint_url,
-                    ),
-                    transform_geo_data.s(event_result.parent.id),
-                )
-                geo_workflow.apply_async()
-
-                impact_workflow = chain(
-                    fetch_event_data.s(
-                        parent_id=gdacs_instance.id,
-                        event_id=event_id,
-                        hazard_type=hazard_type,
-                    ),
-                    transform_impact_data.s(),
-                )
-                impact_workflow.apply_async()
-
-        logger.info(f"{hazard_type} data imported sucessfully")
+    start_date = datetime.date(2000, 1, 1)
+    end_date = datetime.date(2025, 1, 1)
+    for hazard, size in HAZARDS:
+        deep_dive(session, hazard, start_date, end_date, size, "")
--- apps/etl/transform/sources/gdacs.py
+++ apps/etl/transform/sources/gdacs.py
@@ -1,197 +1,44 @@
+import json
 import logging
-import time
-import uuid
 
-from celery import shared_task
-from celery.result import AsyncResult
 from pystac_monty.sources.gdacs import (
     GDACSDataSource,
     GDACSDataSourceType,
     GDACSTransformer,
 )
 
-from apps.etl.models import ExtractionData, PyStacLoadData, Transform, get_trace_id
-from apps.etl.utils import read_file_data
-from main.logging import log_extra
-from main.managers import BulkCreateManager
+from apps.etl.transform.sources.handler import BaseTransformerHandler
+from main.celery import app
 
 logger = logging.getLogger(__name__)
 
 # FIXME: start_end_handler base zzz
 
 
-@shared_task
-def transform_event_data(event_extraction_data):
-    logger.info("Transformation started for event data")
+class GDACSTransformHandler(BaseTransformerHandler[GDACSTransformer, GDACSDataSource]):
+    transformer_class = GDACSTransformer
+    transformer_schema = GDACSDataSource
 
-    gdacs_instance = ExtractionData.objects.get(id=event_extraction_data["extraction_id"])
+    @classmethod
+    def get_schema_data(cls, extraction_object):
+        data = extraction_object.resp_data.read()
+        episodes = []
+        event_objects = extraction_object.child_extractions.all()
+        for episode_obj in event_objects:
+            episodes_data_dict = {}
+            event_episode_data = episode_obj.resp_data.read()
+            episodes_data_dict[GDACSDataSourceType.EVENT] = (episode_obj.url, json.loads(event_episode_data))
+            geometry_objects = episode_obj.child_extractions.all()
 
-    if not gdacs_instance.resp_data:
-        logger.info("Transformation ended due to no data")
-        return
+            for geometry_detail in geometry_objects:
+                geometry_episode_data = geometry_detail.resp_data.read()
+                episodes_data_dict[GDACSDataSourceType.GEOMETRY] = (geometry_detail.url, json.loads(geometry_episode_data))
 
-    data = read_file_data(gdacs_instance.resp_data)
+            episodes.append(episodes_data_dict)
 
-    transform_obj = Transform.objects.create(
-        extraction=gdacs_instance,
-        status=Transform.Status.PENDING,
-        trace_id=get_trace_id(gdacs_instance),
-    )
+        return cls.transformer_schema(source_url=extraction_object.url, data=json.loads(data), episodes=episodes)
 
-    try:
-        transformer = GDACSTransformer(
-            [GDACSDataSource(type=GDACSDataSourceType.EVENT, source_url=gdacs_instance.url, data=data)],
-            # FIXME: Pass geo-coder as in other sources
-            None,
-        )
-        transformed_event_item = transformer.make_source_event_item()
-        transformed_item_dict = transformed_event_item.to_dict()
-
-        transform_obj.status = Transform.Status.SUCCESS
-        transform_obj.save(update_fields=["status"])
-    except Exception as e:
-        logger.error(
-            "Gdacs transformation failed",
-            exc_info=True,
-            extra=log_extra({"extraction_id": gdacs_instance.id}),
-        )
-
-        transform_obj.status = Transform.Status.FAILED
-        transform_obj.save(update_fields=["status"])
-        raise e
-
-    transformed_item_dict["properties"]["monty:etl_id"] = str(uuid.uuid4())
-    PyStacLoadData.objects.create(
-        transform_id=transform_obj,
-        item_type=PyStacLoadData.ItemType.EVENT,
-        collection_id=transformed_event_item.collection_id,
-        item=transformed_item_dict,
-    )
-
-    # transform_obj.is_loaded = True
-    # transform_obj.save(update_fields=["is_loaded"])
-
-    logger.info("Trandformation ended for event data")
-
-
-@shared_task
-def transform_geo_data(geo_data_extraction_id, event_extraction_id):
-    logger.info("Transformation started for hazard data")
-
-    timeout = 60  # 1 minute
-    start_time = time.time()
-    while True:
-        result = AsyncResult(event_extraction_id)
-        if result.state == "SUCCESS":
-            # Fetch the output of event task
-            event_data = result.result
-            break
-        elif result.state == "FAILURE":
-            raise Exception(f"Fetching event data failed with error: {result.result}")
-        elif time.time() - start_time > timeout:
-            raise TimeoutError("Fetching event data timed out.")
-        time.sleep(1)
-
-    gdacs_instance = ExtractionData.objects.get(id=geo_data_extraction_id)
-
-    data = read_file_data(gdacs_instance.resp_data)
-    transform_obj = Transform.objects.create(
-        extraction=gdacs_instance,
-        status=Transform.Status.PENDING,
-        trace_id=get_trace_id(gdacs_instance),
-    )
-
-    try:
-        transformer = GDACSTransformer(
-            [
-                GDACSDataSource(
-                    type=GDACSDataSourceType.EVENT, source_url=gdacs_instance.url, data=event_data["extracted_data"]
-                ),
-                GDACSDataSource(type=GDACSDataSourceType.GEOMETRY, source_url=gdacs_instance.url, data=data),
-            ],
-            # FIXME: Pass geo-coder as in other sources
-            None,
-        )
-        transformed_hazard_item = transformer.make_hazard_event_item()
-        transformed_item_dict = transformed_hazard_item.to_dict()
-
-        transform_obj.status = Transform.Status.SUCCESS
-        transform_obj.save(update_fields=["status"])
-
-    except Exception as e:
-        logger.error(
-            "Gdacs transformation failed",
-            exc_info=True,
-            extra=log_extra({"extraction_id": gdacs_instance.id}),
-        )
-
-        transform_obj.status = Transform.Status.FAILED
-        transform_obj.save(update_fields=["status"])
-        raise e
-
-    transformed_item_dict["properties"]["monty:etl_id"] = str(uuid.uuid4())
-    PyStacLoadData.objects.create(
-        transform_id=transform_obj,
-        item_type=PyStacLoadData.ItemType.HAZARD,
-        collection_id=transformed_hazard_item.collection_id,
-        item=transformed_item_dict,
-    )
-    # transform_obj.is_loaded = True
-    # transform_obj.save(update_fields=["is_loaded"])
-
-    logger.info("Transformation ended for hazard data")
-
-
-@shared_task
-def transform_impact_data(event_data):
-    logger.info("Transformation started for impact data")
-
-    gdacs_instance = ExtractionData.objects.get(id=event_data["extraction_id"])
-    data = read_file_data(gdacs_instance.resp_data)
-
-    transform_obj = Transform.objects.create(
-        extraction=gdacs_instance,
-        status=Transform.Status.PENDING,
-        trace_id=get_trace_id(gdacs_instance),
-    )
-
-    bulk_mgr = BulkCreateManager(chunk_size=1000)
-    try:
-        transformer = GDACSTransformer(
-            [GDACSDataSource(type=GDACSDataSourceType.EVENT, source_url=gdacs_instance.url, data=data)],
-            # FIXME: Pass geo-coder as in other sources
-            None,
-        )
-        transformed_impact_item = transformer.make_impact_items()
-
-        transform_obj.status = Transform.Status.SUCCESS
-        transform_obj.save(update_fields=["status"])
-    except Exception as e:
-        logger.error(
-            "Gdacs transformation failed",
-            exc_info=True,
-            extra=log_extra({"extraction_id": gdacs_instance.id}),
-        )
-
-        transform_obj.status = Transform.Status.FAILED
-        transform_obj.save(update_fields=["status"])
-        raise e
-
-    for item in transformed_impact_item:
-        transformed_item_dict = item.to_dict()
-        transformed_item_dict["properties"]["monty:etl_id"] = str(uuid.uuid4())
-        bulk_mgr.add(
-            PyStacLoadData(
-                transform_id=transform_obj,
-                item_type=PyStacLoadData.ItemType.IMPACT,
-                collection_id=item.collection_id,
-                item=transformed_item_dict,
-            )
-        )
-
-    bulk_mgr.done()
-
-    # transform_obj.is_loaded = True
-    # transform_obj.save(update_fields=["is_loaded"])
-
-    logger.info("Transformation ended for impact data")
+    @staticmethod
+    @app.task
+    def task(extraction_id):
+        GDACSTransformHandler().handle_transformation(extraction_id)
