903dc585d8dcd9db23eae61f1e3910b6fbbe3691
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2025-04-11 18:12:58 +0545

    Fix emdat after rebase.

--- apps/etl/etl_tasks/emdat.py
+++ apps/etl/etl_tasks/emdat.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from celery import chain, shared_task
 
-from apps.etl.extraction.sources.emdat.extract import EMDATExtraction, EMDATQueryVars
+from apps.etl.extraction.sources.emdat.extract import EMDATExtraction, EmdatExtractionInputMetadata
 from apps.etl.transform.sources.emdat import EMDATTransformHandler
 from main.configs import etl_config
 
@@ -83,27 +83,27 @@ query monty(
 def ext_and_transform_emdat_latest_data(**kwargs):
     # FIXME: Why are we getting data from etl_config.EMDAT_START_YEAR to get the latest data?
     # Also, the filtering only filters using year so we might have lot of duplicate data
-    variables: EMDATQueryVars = {
-        "limit": -1,
-        "from": etl_config.EMDAT_START_YEAR,
-        "to": datetime.now().year,
-        "include_hist": None,
-    }
+    metadata = EmdatExtractionInputMetadata(
+        limit=-1,
+        from_=etl_config.EMDAT_START_YEAR,
+        to=datetime.now().year,
+        include_hist=None,
+    )
 
-    chain(EMDATExtraction.task.s(QUERY, variables), EMDATTransformHandler.task.s()).apply_async()
+    chain(EMDATExtraction.task.s(QUERY, metadata.model_dump()), EMDATTransformHandler.task.s()).apply_async()
 
 
 # FIXME: Remove kwargs?
 @shared_task
 def ext_and_transform_emdat_historical_data(**kwargs):
     for i in range(1900, datetime.now().year):
-        variables: EMDATQueryVars = {
-            "limit": -1,
-            "from": i,
-            "to": i,
-            "include_hist": True,
-        }
+        metadata = EmdatExtractionInputMetadata(
+            limit=-1,
+            from_=i,
+            to=i,
+            include_hist=True,
+        )
         _ = chain(
-            EMDATExtraction.task.s(QUERY, variables),
+            EMDATExtraction.task.s(QUERY, metadata.model_dump()),
             EMDATTransformHandler.task.s(),
         ).apply_async()
