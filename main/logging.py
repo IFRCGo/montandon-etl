ffea5a2a9bb54e733a1c37af46f38287a5dec6b4
thenav56 <ayernavin@gmail.com>
2025-04-13 21:18:52 +0545

    feat(logging): Show more info in logging
    - Replace module name with shortname (including parent)
    - Short datetime

--- main/logging.py
+++ main/logging.py
@@ -3,21 +3,28 @@ import logging
 import requests
 
 
-def log_render_extra_context(record: logging.LogRecord):
+def log_render_custom_field(record: logging.LogRecord):
     """
     Append extra->context to logs
     NOTE: This will appear in logs when used with logger.xxx(..., extra={'context': {..content}})
+
+    Add short_name using name
     """
     extra_str = ""
     if extra_raw := getattr(record, "context", None):
         extra_str = f" - EXTRA:{str(extra_raw)}"
     record.context = extra_str
+
+    # Break the logger name into parts
+    parts = record.name.split(".")
+    # Take the last two components (or fewer if not available)
+    record.short_name = ".".join(parts[-2:]) if len(parts) >= 2 else record.name
     return True
 
 
 def log_extra(extra: dict):
     """
-    Basic helper function to view extra argument in logs using log_render_extra_context
+    Basic helper function to view extra argument in logs using log_render_custom_field
     """
     return {
         "context": extra,
