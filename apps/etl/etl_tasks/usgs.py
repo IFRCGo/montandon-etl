f5a652802fb8c355fed488f2aa953050f428edf4
thenav56 <ayernavin@gmail.com>
2025-04-13 21:21:05 +0545

    refactor(usgs): Move extraction logic to handler
    Rate limit is now under same task

--- apps/etl/etl_tasks/usgs.py
+++ apps/etl/etl_tasks/usgs.py
@@ -1,51 +1,12 @@
-import json
 import logging
 from datetime import datetime, timedelta
 
-from celery import chain, shared_task
+from celery import shared_task
 
-from apps.etl.extraction.sources.usgs.extract import USGSExtraction
-from apps.etl.models import ExtractionData
-from apps.etl.transform.sources.usgs import USGSTransformHandler
-from main.celery import CeleryQueue
+from apps.etl.extraction.sources.usgs.extract import USGSExtraction, USGSExtractionMetadata, USGSExtractionMetadataType
 from main.configs import etl_config
-from main.logging import log_extra
 
 logger = logging.getLogger(__name__)
-
-
-@shared_task(queue=CeleryQueue.USGS_EXTRACTION, rate_limit="20/m")  # Note: This is the extraction task  for child class
-def ext_and_transform_usgs_data(url: str):
-    """Extract and Transform USGS data"""
-
-    # Handles base extraction from the all day url
-    base_extraction_id = USGSExtraction.handle_extraction(
-        url=url, params=None, headers={"Content-Type": "application/json"}, source=ExtractionData.Source.USGS
-    )
-
-    if base_extraction_id:
-        instance_id = ExtractionData.objects.get(id=base_extraction_id)
-        response_data = json.loads(instance_id.resp_data.read())
-        # FIXME: We might need to write a simple validator here
-        features_list = response_data["features"]
-
-        BATCH_SIZE = 50
-        for i in range(0, len(features_list), BATCH_SIZE):
-            feature_batch = features_list[i : i + BATCH_SIZE]
-            for feature_item in feature_batch:
-                if "detail" in feature_item["properties"]:
-                    detail_url = feature_item["properties"]["detail"]
-                    chain(
-                        USGSExtraction.task.s(detail_url, base_extraction_id),
-                        USGSTransformHandler.task.s(),
-                    ).apply_async()
-
-    else:
-        logger.error(
-            "Base Extraction ID not found",
-            exc_info=True,
-            extra=log_extra({"extraction_id": base_extraction_id}),
-        )
 
 
 # FIXME: This does not work if one of the system is down for more than a day
@@ -53,10 +14,16 @@ def ext_and_transform_usgs_data(url: str):
 def ext_and_transform_usgs_latest_data():
     """Extract and Transform USGS latest data"""
     url = f"{etl_config.USGS_DATA_URL}/earthquakes/feed/v1.0/summary/all_day.geojson"
-    ext_and_transform_usgs_data(url=url)
+    # url = f"{etl_config.USGS_DATA_URL}/earthquakes/feed/v1.0/summary/all_month.geojson"
+    USGSExtraction.init_extraction(
+        metadata=USGSExtractionMetadata(
+            url=url,
+            type=USGSExtractionMetadataType.QUERY,
+        ),
+    )
 
 
-@shared_task(queue=CeleryQueue.USGS_EXTRACTION, rate_limit="20/m")
+@shared_task
 def ext_and_transform_usgs_historical_data():
     """Extract and Transform USGS historical data"""
     start_date = etl_config.USGS_START_DATE
@@ -69,5 +36,10 @@ def ext_and_transform_usgs_historical_data():
             f"&starttime={start_date.strftime('%Y-%m-%d')}"
             f"&endtime={min(next_date, end_date).strftime('%Y-%m-%d')}"
         )
-        ext_and_transform_usgs_data.s(url=url).apply_async()
+        USGSExtraction.init_extraction(
+            metadata=USGSExtractionMetadata(
+                url=url,
+                type=USGSExtractionMetadataType.QUERY,
+            ),
+        )
         start_date = next_date
