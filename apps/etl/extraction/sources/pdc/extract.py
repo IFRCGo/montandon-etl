01fd56f8113b4d09e4d9d8a1c87aa5cf5aca5b39
Sudan Bhandari <sudan1bhandari@gmail.com>
2025-04-27 22:40:59 +0545

    Update PDC extractions

--- apps/etl/extraction/sources/pdc/extract.py
+++ apps/etl/extraction/sources/pdc/extract.py
@@ -1,20 +1,59 @@
 import json
 import logging
 import typing
+from enum import Enum
 
 import pydantic
 import requests
+from celery import chord
 from django.core.files.base import ContentFile
 
-from apps.etl.extraction.sources.base.handler import BaseExtraction
+from apps.etl.extraction.sources.base.handler import BaseExtraction, BaseExtractionV2
 from apps.etl.models import ExtractionData, HazardType
 from apps.etl.transform.sources.pdc import PDCTransformHandler
 from apps.etl.utils import AccessTokenManager
-from main.celery import CeleryQueue, app
+from main.celery import app
 from main.configs import etl_config
 from main.logging import log_extra
+from utils.celery import RetryableTask
 
 logger = logging.getLogger(__name__)
+
+
+class PDCExtractionMetaDataType(str, Enum):
+    POLYGON = "polygon"
+    EXPOSURE_LIST = "exposure_list"
+    EXPOSURE_DETAIL = "exposure_detail"
+    HAZARD = "hazard"
+
+
+class PDCExtractionMetadata(pydantic.BaseModel):
+    data: typing.Optional[dict] | None = None
+    url: str
+    type: PDCExtractionMetaDataType
+
+
+class HazardEvent(pydantic.BaseModel):
+    app_ID: int
+    app_IDs: str
+    category_ID: str
+    charter_Uri: str
+    comment_Text: typing.Optional[str]
+    create_Date: str
+    end_Date: str
+    glide_Uri: str
+    hazard_ID: int
+    hazard_Name: str
+    last_Update: str
+    latitude: float
+    longitude: float
+    snc_url: str
+    start_Date: str
+    status: str
+    type_ID: str
+    uuid: str
+    in_Dashboard: str
+    description: str
 
 
 class PdcPolygonInputMetadata(pydantic.BaseModel):
@@ -23,9 +62,9 @@ class PdcPolygonInputMetadata(pydantic.BaseModel):
 
 
 class PdcExposureInputMetadata(pydantic.BaseModel):
-    exposure_id: str | int
-    hazard_uuid: str
-    geojson_id: int
+    exposure_id: str | None = None
+    hazard_uuid: str | None = None
+    geojson_id: int | None = None
 
 
 class Pagination(pydantic.BaseModel):
@@ -35,8 +74,8 @@ class Pagination(pydantic.BaseModel):
 
 class Restriction(pydantic.BaseModel):
     searchType: str
-    createDate: typing.Optional[str] = None
-    typeId: typing.Optional[str] = None
+    createDate: typing.Optional[str] | None = None
+    typeId: typing.Optional[str] | None = None
 
 
 class PdcHazardInputMetadata(pydantic.BaseModel):
@@ -290,7 +329,107 @@ class PDCExtraction(BaseExtraction):
                 )
                 raise
 
+
+class PDCExtractionV2(BaseExtractionV2[PDCExtractionMetadata]):
+    """
+    PDC Extraction task.
+    """
+
+    source_enum = ExtractionData.Source.PDC
+    extraction_metadata_class = PDCExtractionMetadata
+
+    def handle_type_hazard(self):
+        self._extraction_fetch_url(
+            self.extraction_metadata.url,
+            data=json.dumps(self.extraction_metadata.data),
+            headers={
+                "Authorization": f"Bearer {etl_config.PDC_SENTRY_AUTHORIZATION_KEY}",
+                "Content-Type": "application/json",
+            },
+            method="post",
+        )
+        response_data = json.loads(self.extraction_object.resp_data.read())
+        for item in response_data:
+            self.init_extraction(
+                metadata=PDCExtractionMetadata(
+                    url=f"{etl_config.PDC_SENTRY_BASE_URL}/hp_srv/services/hazard/{item['uuid']}/exposure",
+                    type=PDCExtractionMetaDataType.EXPOSURE_LIST,
+                    data={"hazard_uuid": item["uuid"], "hazard_id": item["hazard_ID"]},
+                ),
+                parent_extraction=self.extraction_object,
+            )
+
+    def handle_exposure_list(self):
+        self._extraction_fetch_url(
+            self.extraction_metadata.url,
+            headers={
+                "Authorization": f"Bearer {etl_config.PDC_SENTRY_AUTHORIZATION_KEY}",
+                "Content-Type": "application/json",
+            },
+        )
+        response_data = json.loads(self.extraction_object.resp_data.read())
+
+        self.init_extraction(
+            metadata=PDCExtractionMetadata(
+                url=f"{etl_config.PDC_SENTRY_BASE_URL}/hp_srv/services/mags/1/json/get_mags?hazard_id={self.extraction_metadata.data['hazard_id']}",
+                type=PDCExtractionMetaDataType.POLYGON,
+                data={"hazard_id": self.extraction_metadata.data["hazard_id"]},
+            ),
+            parent_extraction=self.extraction_object,
+        )
+
+        exposure_task = []
+        for item in response_data:
+            exposure_extraction_obj = self.init_extraction(
+                metadata=PDCExtractionMetadata(
+                    url=f"{etl_config.PDC_SENTRY_BASE_URL}/hp_srv/services/hazard/{self.extraction_metadata.data['hazard_uuid']}/exposure/{item}",
+                    type=PDCExtractionMetaDataType.EXPOSURE_DETAIL,
+                ),
+                parent_extraction=self.extraction_object.parent.id,
+                add_to_queue=False,
+            )
+            exposure_task.append(PDCExtractionV2.task.si(exposure_extraction_obj.pk))
+        if exposure_task:
+            chord(
+                exposure_task,
+                PDCTransformHandler.task.si(self.extraction_object.pk),
+            ).apply_async()
+        else:
+            PDCTransformHandler.task.delay(self.extraction_object.pk)
+
+    def handle_polygon(self):
+        self._extraction_fetch_url(
+            self.extraction_metadata.url,
+        )
+
+    def handle_exposure_detail(self):
+        self._extraction_fetch_url(
+            self.extraction_metadata.url,
+            headers={
+                "Authorization": f"Bearer {etl_config.PDC_SENTRY_AUTHORIZATION_KEY}",
+                "Content-Type": "application/json",
+            },
+        )
+
+    def handle_extract(self):
+        handler_type = self.extraction_metadata.type
+        logger.info(f"Starting extraction<{self.extraction_object.pk}> with metadata: {self.extraction_metadata}")
+        match handler_type:
+            case PDCExtractionMetaDataType.HAZARD:
+                return self.handle_type_hazard()
+            case PDCExtractionMetaDataType.EXPOSURE_LIST:
+                return self.handle_exposure_list()
+            case PDCExtractionMetaDataType.EXPOSURE_DETAIL:
+                return self.handle_exposure_detail()
+            case PDCExtractionMetaDataType.POLYGON:
+                return self.handle_polygon()
+            case _:
+                typing.assert_never(handler_type)
+
     @staticmethod
-    @app.task(queue=CeleryQueue.EXTRACTION)
-    def task(params: dict):  # type: ignore[reportIncompatibleMethodOverride]
-        return PDCExtraction.handle_extraction(params=params)
+    @app.task(
+        bind=True,
+        base=RetryableTask,
+    )
+    def task(celery_task, extraction_id):
+        PDCExtractionV2(celery_task, extraction_id).handle()
