feea5ba85a4e547ba131963b9499ac3936c20174
sandeshit <thapasandesh56@gmail.com>
2025-04-29 11:28:07 +0545

    Fix pre-commit issues

--- apps/etl/dataloaders.py
+++ apps/etl/dataloaders.py
@@ -1,15 +1,11 @@
 import typing
 
 from asgiref.sync import sync_to_async
+from django.db import models
 from django.utils.functional import cached_property
 from strawberry.dataloader import DataLoader
 
-from django.db import models
-
-from apps.etl.models import ExtractionData,PyStacLoadData
-
-
-
+from apps.etl.models import ExtractionData, PyStacLoadData
 
 DjangoModel = typing.TypeVar("DjangoModel", bound=models.Model)
 
@@ -23,22 +19,23 @@ def load_model_objects(
     return [_map[key] for key in keys]
 
 
-
 if typing.TYPE_CHECKING:
-    from apps.etl.types import ExtractionDataType,PyStacLoadDataType
+    from apps.etl.types import ExtractionDataType, PyStacLoadDataType
 
 
 def load_extraction(keys: list[int]) -> list["ExtractionDataType"]:
     return load_model_objects(ExtractionData, keys)  # type: ignore[reportReturnType]
 
+
 def load_pystac(keys: list[int]) -> list["PyStacLoadDataType"]:
     return load_model_objects(PyStacLoadData, keys)  # type: ignore[reportReturnType]
+
 
 class ExtractionDataLoader:
     @cached_property
     def load_data(self):
         return DataLoader(load_fn=sync_to_async(load_extraction))
-    
+
 
 class PystacDataLoader:
     @cached_property
