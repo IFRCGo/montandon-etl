c4fbd55037bda80aac0227df3270e192531cdd93
sandeshit <thapasandesh56@gmail.com>
2025-04-29 15:39:04 +0545

    Query for all extraction data

--- apps/etl/queries.py
+++ apps/etl/queries.py
@@ -3,12 +3,17 @@ from asgiref.sync import sync_to_async
 from django.db.models import Count, Q
 
 from apps.etl.models import ExtractionData, Status
-from apps.etl.types import StatusCount, StatusSourceCount, ValStatusSourceCount
+from apps.etl.types import RawExtractiondata, StatusCount, StatusSourceCount, ValStatusSourceCount
 from main.graphql.context import Info
 
 
 @strawberry.type
 class PublicQuery:
+    @strawberry.field()
+    async def all_extraction_data(self, info: Info) -> list[RawExtractiondata]:
+        qs = RawExtractiondata.get_queryset(None, None, info).order_by("id")
+        return [data async for data in qs]
+
     @strawberry.field()
     async def total_count(self, info: Info) -> list[StatusCount]:
         query_total_count = await sync_to_async(
