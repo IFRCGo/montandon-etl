76dd6197caa1e8f48e7a20a1f6f24903e5c73f7a
sandeshit <thapasandesh56@gmail.com>
2025-04-29 15:39:04 +0545

    Query for all extraction data

--- apps/etl/types.py
+++ apps/etl/types.py
@@ -1,53 +1,46 @@
 import strawberry
 from django.db import models
 
-from apps.etl.enums import ExtractionSourceTypeEnum
+from apps.etl.enums import ExtractionSourceTypeEnum, ExtractionDataStatusTypeEnum, ExtractionValidationTypeEnum
 from apps.etl.models import ExtractionData
 from main.graphql.context import Info
 from utils.common import get_queryset_for_model
-
+class ExtractionDataQuerysetMixin:
+    @staticmethod
+    def get_queryset(_, queryset: models.QuerySet | None, info: Info):
+        return get_queryset_for_model(ExtractionData, queryset)
 
 @strawberry.type
-class StatusCount:
+class RawExtractiondata(ExtractionDataQuerysetMixin):
+    status: ExtractionDataStatusTypeEnum
+    source: ExtractionSourceTypeEnum
+    source_validation_status: ExtractionValidationTypeEnum
+
+@strawberry.type
+class StatusCount(ExtractionDataQuerysetMixin):
     in_progress_count: int
     success_count: int
     failed_count: int
     pending_count: int
 
-    @staticmethod
-    def get_queryset(_, queryset: models.QuerySet | None, info: Info):
-        new_queryset = get_queryset_for_model(ExtractionData, queryset)
-        return new_queryset
 
-
-@strawberry.type()
-class StatusSourceCount:
+@strawberry.type
+class StatusSourceCount(ExtractionDataQuerysetMixin):
     source: ExtractionSourceTypeEnum
     in_progress_count: int
     success_count: int
     failed_count: int
     pending_count: int
 
-    @staticmethod
-    def get_queryset(_, queryset: models.QuerySet | None, info: Info):
-        new_queryset = get_queryset_for_model(ExtractionData, queryset)
-        return new_queryset
-
 
 @strawberry.type()
-class ValStatusSourceCount:
+class ValStatusSourceCount(ExtractionDataQuerysetMixin):
     source: ExtractionSourceTypeEnum
     success_count: int
     failed_count: int
     no_data_count: int
     no_change_count: int
     no_validation_count: int
-
-    @staticmethod
-    def get_queryset(_, queryset: models.QuerySet | None, info: Info):
-        new_queryset = get_queryset_for_model(ExtractionData, queryset)
-        return new_queryset
-
 
 @strawberry.type()
 class CountbytraceID:
