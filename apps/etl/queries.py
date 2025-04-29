f2a2c62879391ae914684885236087f9f2ba2452
sandeshit <thapasandesh56@gmail.com>
2025-04-29 11:17:56 +0545

    Queries for extraction data

--- apps/etl/queries.py
+++ apps/etl/queries.py
@@ -1,32 +1,82 @@
 import strawberry
 from asgiref.sync import sync_to_async
+from django.db.models import Count, Q
 
-import strawberry_django
-from typing import Optional
+from apps.etl.models import ExtractionData, Status
+from apps.etl.types import StatusCount, StatusSourceCount, ValStatusSourceCount
 from main.graphql.context import Info
-from utils.strawberry.paginations import CountList, pagination_field
-from apps.etl.types import ExtractionDataType,PyStacLoadDataType,EtlTraceGroup
-from apps.etl.filters import ExtractionDataFilter,PyStacLoadDataFilter
-from apps.etl.models import ExtractionData,PyStacLoadData
+
+
 @strawberry.type
 class PublicQuery:
-    extractions: CountList[ExtractionDataType] = pagination_field(
-        pagination=True,
-        filters=ExtractionDataFilter,
-    )
+    @strawberry.field()
+    async def total_count(self, info: Info) -> list[StatusCount]:
+        query_total_count = await sync_to_async(
+            lambda: StatusCount.get_queryset(None, None, info).aggregate(
+                in_progress_count=Count("id", filter=Q(status=Status.IN_PROGRESS)),
+                success_count=Count("id", filter=Q(status=Status.SUCCESS)),
+                failed_count=Count("id", filter=Q(status=Status.FAILED)),
+                pending_count=Count("id", filter=Q(status=Status.PENDING)),
+            )
+        )()
 
-    @strawberry_django.field()
-    async def extraction(self, info: Info, pk: strawberry.ID) -> ExtractionDataType | None:
-        return await ExtractionDataType.get_queryset(None, None, info).filter(pk=pk).afirst()
+        return [
+            StatusCount(
+                in_progress_count=query_total_count["in_progress_count"],
+                success_count=query_total_count["success_count"],
+                failed_count=query_total_count["failed_count"],
+                pending_count=query_total_count["pending_count"],
+            )
+        ]
 
-   
-    pystacdatas: CountList[PyStacLoadDataType] = pagination_field(
-        pagination=True,
-        filters=PyStacLoadDataFilter,
-    )
-    @strawberry_django.field()
-    async def pystacdata(self, info: Info, pk: strawberry.ID) -> PyStacLoadDataType | None:
-        return await PyStacLoadDataType.get_queryset(None, None, info).filter(pk=pk).afirst()
+    @strawberry.field()
+    async def status_source_counts(self, info: Info) -> list[StatusSourceCount]:
+        query_countby_status_source = (
+            StatusSourceCount.get_queryset(None, None, info)
+            .values("source")  # group by source
+            .annotate(
+                in_progress_count=Count("id", filter=Q(status=Status.IN_PROGRESS)),
+                success_count=Count("id", filter=Q(status=Status.SUCCESS)),
+                failed_count=Count("id", filter=Q(status=Status.FAILED)),
+                pending_count=Count("id", filter=Q(status=Status.PENDING)),
+            )
+        )
+        results = await sync_to_async(list)(query_countby_status_source)
 
+        return [
+            StatusSourceCount(
+                source=item["source"],
+                in_progress_count=item["in_progress_count"],
+                success_count=item["success_count"],
+                failed_count=item["failed_count"],
+                pending_count=item["pending_count"],
+            )
+            for item in results
+        ]
 
-    
+    @strawberry.field()
+    async def validation_status_source_counts(self, info: Info) -> list[ValStatusSourceCount]:
+        query_countby_valstatus_source = (
+            ValStatusSourceCount.get_queryset(None, None, info)
+            .values("source")  # group by source
+            .annotate(
+                success_count=Count("id", filter=Q(source_validation_status=ExtractionData.ValidationStatus.SUCCESS)),
+                failed_count=Count("id", filter=Q(source_validation_status=ExtractionData.ValidationStatus.FAILED)),
+                no_data_count=Count("id", filter=Q(source_validation_status=ExtractionData.ValidationStatus.NO_DATA)),
+                no_change_count=Count("id", filter=Q(source_validation_status=ExtractionData.ValidationStatus.NO_CHANGE)),
+                no_validation_count=Count(
+                    "id", filter=Q(source_validation_status=ExtractionData.ValidationStatus.NO_VALIDATION)
+                ),
+            )
+        )
+        return [
+            ValStatusSourceCount(
+                source=event["source"],
+                success_count=event["success_count"],
+                failed_count=event["failed_count"],
+                no_data_count=event["no_data_count"],
+                no_change_count=event["no_change_count"],
+                no_validation_count=event["no_validation_count"],
+            )
+            async for event in query_countby_valstatus_source
+        ]
