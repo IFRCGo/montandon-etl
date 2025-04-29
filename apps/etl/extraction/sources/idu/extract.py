2c05f2364b86cfb39e92fc5a8251f35ba7a7bc6b
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-04-29 15:03:27 +0545

    Refactor the IDU source extraction based on version2

--- apps/etl/extraction/sources/idu/extract.py
+++ apps/etl/extraction/sources/idu/extract.py
@@ -1,24 +1,52 @@
 import logging
+import typing
+from enum import Enum
 
-from apps.etl.extraction.sources.base.handler import BaseExtraction
+import pydantic
+
+from apps.etl.extraction.sources.base.handler import BaseExtractionV2
 from apps.etl.models import ExtractionData
-from main.celery import app
+from main.celery import CeleryQueue, app
 from main.configs import etl_config
+from utils.celery import RetryableTask
 
 logger = logging.getLogger(__name__)
 
 
-class IDUExtraction(BaseExtraction):
+class IDUExtractionMetadataType(str, Enum):
+    QUERY = "QUERY"
+
+
+class IDUExtractionMetadata(pydantic.BaseModel):
+    url: str
+    type: IDUExtractionMetadataType
+
+
+class IDUExtraction(BaseExtractionV2[IDUExtractionMetadata]):
     """
     Handles data extraction from the IDU API.
     """
 
+    source_enum = ExtractionData.Source.IDU
+    extraction_metadata_class = IDUExtractionMetadata
+
+    def _handle_type_query(self):
+        url = self.extraction_metadata.url
+        headers = {"Content-Type": "application/json"}
+        params = {"client_id": etl_config.IDMC_CLIENT_ID}
+        self._extraction_fetch_url(url, headers=headers, params=params)
+        return self.extraction_object.id
+
+    def handle_extract(self):
+        handler_type = self.extraction_metadata.type
+        logger.info(f"Starting extraction<{self.extraction_object.pk}> with metadata: {self.extraction_metadata}")
+        match handler_type:
+            case IDUExtractionMetadataType.QUERY:
+                return self._handle_type_query()
+            case _:
+                typing.assert_never(handler_type)
+
     @staticmethod
-    @app.task
-    def task(url: str):  # type: ignore[reportIncompatibleMethodOverride]
-        return IDUExtraction.handle_extraction(
-            url=url,
-            params={"client_id": etl_config.IDMC_CLIENT_ID},
-            headers={"accept": "application/json"},
-            source=ExtractionData.Source.IDU.value,
-        )
+    @app.task(bind=True, base=RetryableTask, queue=CeleryQueue.DEFAULT)
+    def task(celery_task, extraction_id):  # type: ignore[reportIncompatibleMethodOverride]
+        return IDUExtraction(celery_task, extraction_id).handle()
