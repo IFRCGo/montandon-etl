68065c2ea39feddf4b5a79ad1ed287118f4bf841
sandeshit <thapasandesh56@gmail.com>
2025-04-30 11:38:44 +0545

    Rename the enums

--- apps/etl/filters.py
+++ apps/etl/filters.py
@@ -2,8 +2,8 @@ import strawberry
 import strawberry_django
 
 from apps.etl.enums import (
-    ExtractionDataStatusTypeEnum,
-    ExtractionSourceTypeEnum,
+    DataStatusTypeEnum,
+    SourceTypeEnum,
 )
 from apps.etl.models import ExtractionData
 
@@ -11,5 +11,5 @@ from apps.etl.models import ExtractionData
 @strawberry_django.filters.filter(ExtractionData, lookups=True)
 class ExtractionDataFilter:
     created_at: strawberry.auto
-    source: ExtractionSourceTypeEnum
-    status: ExtractionDataStatusTypeEnum
+    source: SourceTypeEnum
+    status: DataStatusTypeEnum
