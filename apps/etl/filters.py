6edba0c05242551fd5e2e5aca7b7e7ee39abc76c
sandeshit <thapasandesh56@gmail.com>
2025-04-30 16:26:44 +0545

    (Fix):pagination query for all three tables

--- apps/etl/filters.py
+++ apps/etl/filters.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import strawberry
 import strawberry_django
 
@@ -11,5 +13,7 @@ from apps.etl.models import ExtractionData
 @strawberry_django.filters.filter(ExtractionData, lookups=True)
 class ExtractionDataFilter:
     created_at: strawberry.auto
-    source: SourceTypeEnum
-    status: DataStatusTypeEnum
+    source: Optional[SourceTypeEnum]
+    status: Optional[DataStatusTypeEnum]
+    trace_id: strawberry.auto
+    created_at: strawberry.auto
