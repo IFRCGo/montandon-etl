f3d63677fa6e722e40cd731c14cda3d2937b57b8
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-04-30 09:55:10 +0545

    Set add queue to False
    Set add queue to False

--- apps/etl/etl_tasks/idu.py
+++ apps/etl/etl_tasks/idu.py
@@ -14,7 +14,7 @@ def ext_and_transform_idu_historical_data():
     """Extract and Transform IDU historical data"""
     url = f"{etl_config.IDMC_DATA_URL}/external-api/idus/all/"
     extraction_obj = IDUExtraction.init_extraction(
-        metadata=IDUExtractionMetadata(url=url, type=IDUExtractionMetadataType.QUERY)
+        metadata=IDUExtractionMetadata(url=url, type=IDUExtractionMetadataType.QUERY), add_to_queue=False
     )
     chain(IDUExtraction.task.s(extraction_obj.id), IDUTransformHandler.task.s()).apply_async()
 
@@ -25,6 +25,6 @@ def ext_and_transform_idu_latest_data():
     url = f"{etl_config.IDMC_DATA_URL}/external-api/idus/last-180-days/"
 
     extraction_obj = IDUExtraction.init_extraction(
-        metadata=IDUExtractionMetadata(url=url, type=IDUExtractionMetadataType.QUERY)
+        metadata=IDUExtractionMetadata(url=url, type=IDUExtractionMetadataType.QUERY), add_to_queue=False
     )
-    chain(IDUExtraction.task.s(extraction_obj.id), IDUTransformHandler.task.s(extraction_obj.id)).apply_async()
+    chain(IDUExtraction.task.s(extraction_obj.id), IDUTransformHandler.task.s()).apply_async()
