c383c31163cdbb129a219bb2b6677d4854e6c560
sandeshit <thapasandesh56@gmail.com>
2025-05-08 11:01:32 +0545

    feat(app): add user app and model

--- main/settings.py
+++ main/settings.py
@@ -194,6 +194,7 @@ INSTALLED_APPS = [
     # Internal
     "apps.common",
     "apps.etl",
+    "apps.user",
 ]
 
 MIDDLEWARE = [
@@ -266,7 +267,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
