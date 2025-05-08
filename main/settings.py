74931239eff15cca1d0ba76379a96f0e4f836b81
sudip-khanal <sudipkhanal0777@gmail.com>
2025-05-08 16:06:50 +0545

    fix user migrations error

--- main/settings.py
+++ main/settings.py
@@ -190,6 +190,8 @@ INSTALLED_APPS = [
     # Internal
     "apps.common",
     "apps.etl",
+    # user
+    "apps.user"
 ]
 
 MIDDLEWARE = [
@@ -255,7 +257,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
+AUTH_USER_MODEL ="user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
