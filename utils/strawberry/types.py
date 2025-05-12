bf6e7665ed83af9ecc859c317e0c4f5c6501b008
sandeshit <thapasandesh56@gmail.com>
2025-05-12 16:05:32 +0545

    chore(strawberry): update base util files

--- utils/strawberry/types.py
+++ utils/strawberry/types.py
@@ -1,11 +1,13 @@
-import json
 import typing
 
 import strawberry
-from django.contrib.gis.geos import GEOSGeometry
+from django.core.files.storage import FileSystemStorage, default_storage
 from django.db import models
 from django.db.models.fields import Field as DjangoBaseField
-from modeltranslation.fields import TranslationFieldDescriptor
+from django.db.models.fields import files
+from strawberry_django.fields.types import field_type_map
+
+from main.graphql.context import Info
 
 if typing.TYPE_CHECKING:
     from django.db.models.fields import _FieldDescriptor
@@ -16,25 +18,6 @@ GenericScalar = strawberry.scalar(
     description="The GenericScalar scalar type represents a generic GraphQL scalar value that could be: List or Object.",
     serialize=lambda v: v,
     parse_value=lambda v: v,
-)
-
-
-class GIS:
-    @staticmethod
-    def serialize(geometry):
-        return json.loads(geometry.geojson)
-
-    @classmethod
-    def parse_value(cls, node):
-        geometry = GEOSGeometry(node.value)
-        return json.loads(geometry.geojson)
-
-
-PolygonScalar = strawberry.scalar(
-    typing.NewType("PolygonScalar", typing.Any),  # type: ignore[reportGeneralTypeIssues]
-    description="",  # TODO: Add description
-    serialize=GIS.serialize,
-    parse_value=GIS.parse_value,
 )
 
 
@@ -58,8 +41,6 @@ def string_field(
     _field = field
     if isinstance(field, models.query_utils.DeferredAttribute):
         _field = field.field
-    elif isinstance(field, TranslationFieldDescriptor):
-        _field = field.field
 
     def _get_value(root) -> None | str:
         return getattr(root, _field.attname)  # type: ignore[reportGeneralTypeIssues] FIXME
@@ -69,12 +50,41 @@ def string_field(
         return _get_value(root)  # type: ignore[reportGeneralTypeIssues] FIXME
 
     @strawberry.field
-    def nullable_string_(root) -> typing.Optional[str]:
+    def nullable_string_(root) -> str | None:
         _value = _get_value(root)
         if _value == "":
-            return
+            return None
         return _value
 
     if _field.null or _field.blank:  # type: ignore[reportGeneralTypeIssues] FIXME
         return nullable_string_
     return string_
+
+
+@strawberry.type
+class DjangoFileType:
+    name: str
+    size: int
+
+    @strawberry.field
+    @staticmethod
+    def url(root: files.FieldFile, info: Info) -> str:
+        # TODO: Use cache if using S3 URL with signature
+        if isinstance(default_storage, FileSystemStorage):
+            return info.context.request.build_absolute_uri(root.url)
+        return root.url
+
+
+@strawberry.type
+class DjangoImageType(DjangoFileType):
+    width: int
+    height: int
+
+
+# Update the strawberry django field type mapping
+field_type_map.update(
+    {
+        files.FileField: DjangoFileType,
+        files.ImageField: DjangoImageType,
+    },
+)
