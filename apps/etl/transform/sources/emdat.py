24edcd9b60a2fc6ce85b8d7c74215aa23f38a7c9
rup <rup.rajbanshi@togglecorp.com>
2025-05-19 18:20:22 +0545

    add file as input inplace of data in transformer

--- apps/etl/transform/sources/emdat.py
+++ apps/etl/transform/sources/emdat.py
@@ -1,10 +1,10 @@
-import json
 import logging
 
 from pystac_monty.sources.emdat import EMDATDataSource, EMDATTransformer
 
 from apps.etl.models import ExtractionData
 from apps.etl.transform.sources.handler import BaseTransformerHandler
+from apps.etl.utils import write_into_temp_file
 from main.celery import app
 
 logger = logging.getLogger(__name__)
@@ -16,12 +16,13 @@ class EMDATTransformHandler(BaseTransformerHandler[EMDATTransformer, EMDATDataSo
 
     @classmethod
     def get_schema_data(cls, extraction_obj: ExtractionData):
-        with extraction_obj.resp_data.open() as file_data:
-            data = json.loads(file_data.read())
+        with extraction_obj.resp_data.open("rb") as f:
+            data = f.read()
+        data_file = write_into_temp_file(data)
 
         return cls.transformer_schema(
             source_url=extraction_obj.url,
-            data=data,
+            data=data_file.name,
         )
 
     @staticmethod
