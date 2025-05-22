aff9c00a4932bf1932ae2862ffd771f264a6b138
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-05-22 14:43:44 +0545

    Refactor Data source for the IDUDataSourceV2
    Fix lint

--- apps/etl/transform/sources/idu.py
+++ apps/etl/transform/sources/idu.py
@@ -1,19 +1,25 @@
-from pystac_monty.sources.idu import IDUDataSource, IDUTransformer
+from pystac_monty.sources.common import DataType, File
+from pystac_monty.sources.idu import IDUDataSourceV2, IDUTransformer
 
 from apps.etl.transform.sources.handler import BaseTransformerHandler
+from apps.etl.utils import write_into_temp_file
 from main.celery import CeleryQueue, app
 
 
-class IDUTransformHandler(BaseTransformerHandler[IDUTransformer, IDUDataSource]):
+class IDUTransformHandler(BaseTransformerHandler[IDUTransformer, IDUDataSourceV2]):
     transformer_class = IDUTransformer
-    transformer_schema = IDUDataSource
+    transformer_schema = IDUDataSourceV2
 
     @classmethod
     def get_schema_data(cls, extraction_obj):
-        with extraction_obj.resp_data.open() as file_data:
-            data = file_data.read()
+        with extraction_obj.resp_data.open("rb") as file_data:
+            file_content = file_data.read()
 
-        return cls.transformer_schema(source_url=extraction_obj.url, data=data)
+        data_file = write_into_temp_file(content=file_content)
+
+        data_source = {"source_url": extraction_obj.url, "source_data": File(path=data_file.name, data_type=DataType.FILE)}
+
+        return cls.transformer_schema(data_source)
 
     @staticmethod
     @app.task(queue=CeleryQueue.DEFAULT)
