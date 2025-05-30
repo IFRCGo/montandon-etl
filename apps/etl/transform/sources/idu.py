cb07296235e0b8e61ae14c62e1549d4f95b9a457
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-05-30 14:11:07 +0545

    Use the updated file/memory schema

--- apps/etl/transform/sources/idu.py
+++ apps/etl/transform/sources/idu.py
@@ -1,14 +1,14 @@
-from pystac_monty.sources.common import DataType, File
-from pystac_monty.sources.idu import IDUDataSourceV2, IDUTransformer
+from pystac_monty.sources.common import DataType, File, GenericDataSource
+from pystac_monty.sources.idu import IDUDataSource, IDUTransformer
 
 from apps.etl.transform.sources.handler import BaseTransformerHandler
 from apps.etl.utils import write_into_temp_file
 from main.celery import CeleryQueue, app
 
 
-class IDUTransformHandler(BaseTransformerHandler[IDUTransformer, IDUDataSourceV2]):
+class IDUTransformHandler(BaseTransformerHandler[IDUTransformer, IDUDataSource]):
     transformer_class = IDUTransformer
-    transformer_schema = IDUDataSourceV2
+    transformer_schema = IDUDataSource
 
     @classmethod
     def get_schema_data(cls, extraction_obj):
@@ -17,8 +17,9 @@ class IDUTransformHandler(BaseTransformerHandler[IDUTransformer, IDUDataSourceV2
 
         data_file = write_into_temp_file(content=file_content)
 
-        data_source = {"source_url": extraction_obj.url, "source_data": File(path=data_file.name, data_type=DataType.FILE)}
-
+        data_source = GenericDataSource(
+            source_url=extraction_obj.url, input_data=File(path=data_file.name, data_type=DataType.FILE)
+        )
         return cls.transformer_schema(data_source)
 
     @staticmethod
