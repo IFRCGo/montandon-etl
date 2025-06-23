1eaca08d9b273ee466446423abecb3cf12d5d68c
Sudan Bhandari <sudan1bhandari@gmail.com>
2025-06-23 15:10:55 +0545

    fixup! chore(schema) update schema extractionId, status

--- apps/etl/types.py
+++ apps/etl/types.py
@@ -29,7 +29,7 @@ class PyStacDataQuerysetMixin:
 
 @strawberry_django.type(ExtractionData)
 class RawExtractiondatatype:
-    id: auto = strawberry_django.field(name="extractionId")
+    id: auto
     source: SourceTypeEnum
     status: DataStatusTypeEnum
     url: auto
@@ -43,7 +43,7 @@ class RawExtractiondatatype:
 
 @strawberry_django.type(Transform)
 class RawTransformdatatype:
-    id: auto = strawberry_django.field(name="transformId")
+    id: auto
     status: DataStatusTypeEnum
     trace_id: auto = strawberry_django.field(only=["trace_id"])
     metadata: auto
