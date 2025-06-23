3fd23874cbb8607274883eceef89b1ee41937a3c
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2025-06-23 16:16:05 +0545

    chore: add response type for retrigger mutation

--- apps/etl/extraction/sources/noaa_IBTrACS/extract.py
+++ apps/etl/extraction/sources/noaa_IBTrACS/extract.py
@@ -37,7 +37,7 @@ class IBTrACSExtraction(BaseExtractionV2[IBTrACSExtractionMetadata]):
         )
         IbtracsTransformHandler.task.delay(self.extraction_object.id)
 
-    def handle_extract(self):
+    def handle_extract(self, retrigger: bool = False):
         handler_type = self.extraction_metadata.type
         logger.info(f"Starting extraction<{self.extraction_object.pk}> with metadata: {self.extraction_metadata}")
         match handler_type:
