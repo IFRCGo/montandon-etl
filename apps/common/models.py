78292df6cf27dbbf384fb3fa15fb690e5c7b82e2
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2025-06-25 14:03:44 +0545

    fix(pdc): rename pdc latest extraction function
      - Disaggregate pdc data according to event

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# from apps.user.models import User
-
 
 class Resource(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,30 +11,3 @@ class Resource(models.Model):
     class Meta:
         abstract = True
         ordering = ["-id"]
-
-
-# class UserResource(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     modified_at = models.DateTimeField(auto_now=True)
-#     created_by = models.ForeignKey(
-#         User,
-#         related_name="%(class)s_created",
-#         on_delete=models.PROTECT,
-#     )
-#     modified_by = models.ForeignKey(
-#         User,
-#         related_name="%(class)s_modified",
-#         on_delete=models.PROTECT,
-#     )
-
-#     # Typing
-#     created_by_id: int
-#     modified_by_id: int
-
-#     class Meta:  # type: ignore[reportIncompatibleVariableOverride]
-#         abstract: bool = True
-#         ordering: list[str] = ["-id"]
-
-#     @typing.override
-#     def __str__(self):
-#         return str(self.pk)
