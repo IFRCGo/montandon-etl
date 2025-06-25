764afb0abae01fc89026dbc5ee9f2d2947861e40
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-06-25 10:30:24 +0545

    Refactor the start_date and end_date handling logic

--- apps/etl/management/commands/extract_gdacs_data.py
+++ apps/etl/management/commands/extract_gdacs_data.py
@@ -1,35 +1,23 @@
-import logging
-from datetime import datetime
-
 from django.core.management.base import BaseCommand, CommandParser
 
 from apps.etl.etl_tasks.gdacs import ext_and_transform_gdacs_historical_data
-
-logger = logging.getLogger(__name__)
+from apps.etl.management.commands.utils import validate_dates
 
 
 class Command(BaseCommand):
     help = "Import data from gdacs api"
 
     def add_arguments(self, parser: CommandParser) -> None:
+        """Set arguments"""
         parser.add_argument("start_date", type=str, help="Format: YYYY-MM-DD")
         parser.add_argument("end_date", type=str, help="Format: YYYY-MM-DD")
 
     def handle(self, *args, **options):
-        start_date: str = options.get("start-date")
-        end_date: str = options.get("end-date")
+        """Handler"""
+        start_date: str | None = options.get("start_date")
+        end_date: str | None = options.get("end_date")
 
-        try:
-            if start_date and end_date:
-                dt_start_date = datetime.strptime(start_date, "%Y-%m-%d")
-                dt_end_date = datetime.strptime(end_date, "%Y-%m-%d")
-                if dt_start_date > dt_end_date:
-                    logger.error("Start date cannot be at a later date with respect to End date")
-                    return
-            else:
-                logger.error("Start date or End date is not specified correctly")
-                return
-        except Exception:
-            logger.error("Error occurred while processing the Start/End dates")
-            return
-        ext_and_transform_gdacs_historical_data(start_date=dt_start_date, end_date=dt_end_date)
+        dt_start_date, dt_end_date = validate_dates(start_date=start_date, end_date=end_date)
+
+        if dt_start_date and dt_end_date:
+            ext_and_transform_gdacs_historical_data(start_date=dt_start_date, end_date=dt_end_date)
