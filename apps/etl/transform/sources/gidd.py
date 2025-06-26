e77c4f696028808e1c48cbfed65ff419c574370b
Sudan bhandari <54224447+sudan45@users.noreply.github.com>
2025-06-26 10:56:51 +0545

    feat(source): remove tmp file (#299)
    * feat(source): remove tmp file
    
    * fix: delete temp file after transformation.
    
    * fix(sources): remove tmp files
    
    ---------
    
    Co-authored-by: rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>

--- apps/etl/transform/sources/gidd.py
+++ apps/etl/transform/sources/gidd.py
@@ -21,7 +21,10 @@ class GIDDTransformHandler(BaseTransformerHandler[GIDDTransformer, GIDDDataSourc
             source_url=extraction_obj.url, input_data=File(path=data_file.name, data_type=DataType.FILE)
         )
 
-        return cls.transformer_schema(data_source)
+        result = cls.transformer_schema(data_source)
+
+        tmp_files = [data_file]
+        return result, tmp_files
 
     @staticmethod
     @app.task(queue=CeleryQueue.DEFAULT)
