0aead5bded4ff4b13f9afc9a183593c853554459
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2025-07-01 15:12:16 +0545

    fix: fix type for trace_id, and transform_id in retrigger api

--- apps/etl/mutations.py
+++ apps/etl/mutations.py
@@ -95,7 +95,7 @@ class Mutation:
 
 def run_transform_retrigger(data: TransformRetriggerInput) -> None:
     logger.info("Transform retrigger processing")
-    failed_transform_objects = Transform.objects.filter(id__in=data.transform_id, status=Transform.Status.FAILED)
+    failed_transform_objects = Transform.objects.filter(id__in=data.transform_ids, status=Transform.Status.FAILED)
 
     for obj in failed_transform_objects:
         transform_class = source_transform_map[obj.extraction.source]
@@ -106,7 +106,7 @@ def run_pipeline_retrigger(data: PipelineRetriggerInput) -> None:
     logger.info("Pipeline retrigger processing")
 
     failed_extraction_objects = ExtractionData.objects.filter(
-        trace_id__in=data.trace_id, status=ExtractionData.Status.FAILED
+        trace_id__in=data.trace_ids, status=ExtractionData.Status.FAILED
     )
 
     for obj in failed_extraction_objects:
