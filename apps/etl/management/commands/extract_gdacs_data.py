ae4290ba41861ede6b39fd83b81014d4a92e458b
Sudan Bhandari <sudan1bhandari@gmail.com>
2025-07-25 11:38:33 +0545

    fix(gdacs): update geometry from retrigger

--- apps/etl/management/commands/extract_gdacs_data.py
+++ apps/etl/management/commands/extract_gdacs_data.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from apps.etl.etl_tasks.gdacs import ext_and_transform_gdacs_latest_data
+from apps.etl.etl_tasks.gdacs import ext_and_transform_gdacs_historical_data
 
 
 class Command(BaseCommand):
     help = "Import data from gdacs api"
 
     def handle(self, *args, **options):
-        ext_and_transform_gdacs_latest_data()
+        ext_and_transform_gdacs_historical_data()
