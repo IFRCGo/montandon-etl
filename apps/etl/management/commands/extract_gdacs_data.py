a2caf99ed59d84a8a92581f28f2c165c34a6c763
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-08-13 10:41:31 +0545

    Fix(GDACS): Update the handler that gets the date inputs

--- apps/etl/management/commands/extract_gdacs_data.py
+++ apps/etl/management/commands/extract_gdacs_data.py
@@ -1,23 +1,32 @@
 from django.core.management.base import BaseCommand, CommandParser
 
 from apps.etl.etl_tasks.gdacs import ext_and_transform_gdacs_historical_data
-from apps.etl.management.commands.utils import validate_dates
+from apps.etl.management.commands.utils import validate_date_format
 
 
 class Command(BaseCommand):
     help = "Import data from gdacs api"
 
+    def confirm(self, message: str) -> bool:
+        prompt = self.style.NOTICE(f"{message} (y/n): ")
+        return input(prompt).strip().lower() == "y"
+
     def add_arguments(self, parser: CommandParser) -> None:
         """Set arguments"""
-        parser.add_argument("start_date", type=str, help="Format: YYYY-MM-DD")
-        parser.add_argument("end_date", type=str, help="Format: YYYY-MM-DD")
+        parser.add_argument("--start-date", required=True, type=validate_date_format, help="Start date in YYYY-MM-DD format")
+        parser.add_argument("--end-date", required=True, type=validate_date_format, help="End date in YYYY-MM-DD format")
 
     def handle(self, *args, **options):
         """Handler"""
         start_date: str | None = options.get("start_date")
         end_date: str | None = options.get("end_date")
 
-        dt_start_date, dt_end_date = validate_dates(start_date=start_date, end_date=end_date)
+        confirm_message = (
+            f"Are you sure? This will trigger IFRC import\nFROM:\t{start_date}\nTO:\t{end_date}\nPlease confirm"
+        )
+        if not self.confirm(confirm_message):
+            self.stderr.write(self.style.ERROR("Skipped...."))
+            return
 
-        if dt_start_date and dt_end_date:
-            ext_and_transform_gdacs_historical_data(start_date=dt_start_date, end_date=dt_end_date)
+        ext_and_transform_gdacs_historical_data(start_date=start_date, end_date=end_date)
+        self.stdout.write(self.style.SUCCESS("Triggered successfully"))
