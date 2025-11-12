244fadeaccc91fdca21dcddb50c578a16c1900ca
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-11-12 23:01:01 +0545

    Testcase: update the test cases for all sources

--- apps/etl/tests/sources/usgs_test.py
+++ apps/etl/tests/sources/usgs_test.py
@@ -80,7 +80,7 @@ def test_handle_extraction_various_usgs_files(case):
     with open(expected_path, "r", encoding="utf-8") as f:
         expected_json = json5.load(f)
 
-    ignored_keys = {"created_at", "modified_at", "monty:etl_id", "pk", "trace", "transform_id", "href"}
+    ignored_keys = {"created_at", "modified_at", "monty:etl_id", "pk", "trace", "transform_id", "href", "keywords"}
     actual_filtered = remove_ignored_keys(actual_json, ignored_keys)
     expected_filtered = remove_ignored_keys(expected_json, ignored_keys)
 
