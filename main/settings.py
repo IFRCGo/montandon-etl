9fb1cf7694f060e295945349ec716edacaf096aa
thenav56 <ayernavin@gmail.com>
2025-11-20 11:32:32 +0545

    fix: remove unused local cache storage

--- main/settings.py
+++ main/settings.py
@@ -331,7 +331,6 @@ STATICFILES_DIRS = [
     str(BASE_DIR.joinpath("apps/static")),
 ]
 
-LOCAL_CACHE_DATA_DIR = "/local-cache-data"
 STATIC_URL = env("DJANGO_STATIC_URL")
 MEDIA_URL = env("DJANGO_MEDIA_URL")
 
