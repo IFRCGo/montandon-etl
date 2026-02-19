5365510856e4a8c4018ada4f0f9a12c8a9f367c1
sandeshit <thapasandesh56@gmail.com>
2026-02-19 15:23:22 +0545

    transformer(alert-hub): add transformer config

--- apps/etl/transform/sources/handler.py
+++ apps/etl/transform/sources/handler.py
@@ -47,6 +47,9 @@ ITEM_TYPE_COLLECTION_ID_MAP = {
     "gdacs-events": PyStacLoadData.ItemType.EVENT,
     "gdacs-impacts": PyStacLoadData.ItemType.IMPACT,
     "gdacs-hazards": PyStacLoadData.ItemType.HAZARD,
+    "alerthub-events": PyStacLoadData.ItemType.EVENT,
+    "alerthub-impacts": PyStacLoadData.ItemType.IMPACT,
+    "alerthub-hazards": PyStacLoadData.ItemType.HAZARD,
 }
 
 
